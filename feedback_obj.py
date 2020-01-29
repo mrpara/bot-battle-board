e3983a37d9c1bcf53cc005e5d847a4a9485b8424
mrpara <60187496+mrpara@users.noreply.github.com>
2020-01-29 23:18:34 +0200

    add option to write match record to file

--- feedback_obj.py
+++ feedback_obj.py
@@ -10,18 +10,23 @@ class Singleton(type):
 
 class Feedback(metaclass=Singleton):
     # This object is in charge of displaying messages ("unit x attacked" etc) and board state
-    def __init__(self, display_messages=True, display_board=True):
+    def __init__(self, display_messages=True, display_board=True, write_to_file=False, file_path="log.txt"):
         self.display_messages = display_messages
         self.display_board = display_board
+        self.write_to_file = write_to_file
+        self.file_path = file_path
+        if write_to_file is True:
+            open(self.file_path, 'w').close()
 
     def display_message(self, message):
         if self.display_messages is True:
             print(message)
+        self.to_file(message + '\n')
 
     def print_board(self, board):
         # Print the board_matrix matrix nicely formatted
         # Code taken from https://stackoverflow.com/questions/13214809/pretty-print-2d-python-list/32159502
-        if self.display_board is True:
+        if self.display_board is True or self.write_to_file is True:
             output_mtx = []
             for row in board.board_matrix:
                 output_mtx.append(['X' if elem is None else elem.id for elem in row])
@@ -29,4 +34,15 @@ class Feedback(metaclass=Singleton):
             lens = [max(map(len, col)) for col in zip(*s)]
             fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
             table = [fmt.format(*row) for row in s]
-            print('\n'.join(table))
+            table_formatted = '\n'.join(table)
+
+            if self.display_board is True:
+                print(table_formatted)
+            if self.write_to_file is True:
+                self.to_file(table_formatted + '\n')
+
+    def to_file(self, message):
+        if self.write_to_file is not True:
+            return
+        with open(self.file_path, 'a') as file:
+            file.write(message)
